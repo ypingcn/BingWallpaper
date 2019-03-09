41c9ec4c407bc88e7e6966dbbf863d291249eeb8
ypingcn <1344632698@qq.com>
2019-03-09 15:12:11 +0800

    chore: update i18n

--- OneClickBingWallpaperTranslation.h
+++ OneClickBingWallpaperTranslation.h
@@ -5,19 +5,22 @@
 
 void GenerateSettingTranslate()
 {
-    auto base = QObject::tr("Basic");                      // base
-    auto baseUpdate = QObject::tr("Update");               // base.update
-    auto baseUpdateType = QObject::tr("Type");             // base.update.type
-    auto baseNotification = QObject::tr("Notification");   // base.notification
-    auto baseDomain = QObject::tr("Domain");               // base.domain
-    auto baseAutoupdate = QObject::tr("Auto Update");      // base.autoupdate
-    auto baseAutoupdateInterval = QObject::tr("Interval"); // base.autoupdate.interval
-    auto baseSecurity = QObject::tr("Security");           // base.security
+    auto base = QObject::tr("Basic");                            // base
+    auto baseUpdate = QObject::tr("Update");                     // base.update
+    auto baseUpdateType = QObject::tr("Type");                   // base.update.type
+    auto baseUpdateDesktop = QObject::tr("Desktop Environment"); // base.update.desktop
+    auto baseNotification = QObject::tr("Notification");         // base.notification
+    auto baseDomain = QObject::tr("Domain");                     // base.domain
+    auto baseAutoupdate = QObject::tr("Auto Update");            // base.autoupdate
+    auto baseAutoupdateInterval = QObject::tr("Interval");       // base.autoupdate.interval
+    auto baseSecurity = QObject::tr("Security");                 // base.security
+    auto baseOther = QObject::tr("Other");                       // base.other
 
     auto baseNotificationText = QObject::tr("Send Notification");
     auto baseDomainSubdomainText = QObject::tr("Subdomain for bing.com");
     auto baseAutoupdateStartupenableText = QObject::tr("Autoupdate Bingwallpaper when tool startup");
     auto baseSecurityPythoncheckText = QObject::tr("Check setting Python file's MD5");
+    auto baseOtherLanguageUpdateHintText = QObject::tr("Show hint of restart for updating language")
 
     auto restoreDefaults = QObject::tr("Restore Defaults");
 }
--- i18n/zh-CN.ts
+++ i18n/zh-CN.ts
@@ -4,117 +4,123 @@
 <context>
     <name>OneClickBingWallpaper</name>
     <message>
-        <location filename="../OneClickBingWallpaper.cpp" line="86"/>
+        <location filename="../OneClickBingWallpaper.cpp" line="88"/>
         <source>Update</source>
         <translation>更新壁纸</translation>
     </message>
     <message>
-        <location filename="../OneClickBingWallpaper.cpp" line="88"/>
+        <location filename="../OneClickBingWallpaper.cpp" line="90"/>
         <source>Setting</source>
         <translation>设置</translation>
     </message>
     <message>
-        <location filename="../OneClickBingWallpaper.cpp" line="90"/>
+        <location filename="../OneClickBingWallpaper.cpp" line="92"/>
         <source>About</source>
         <translation>关于</translation>
     </message>
     <message>
-        <location filename="../OneClickBingWallpaper.cpp" line="92"/>
+        <location filename="../OneClickBingWallpaper.cpp" line="94"/>
         <source>Quit</source>
         <translation>退出</translation>
     </message>
     <message>
-        <location filename="../OneClickBingWallpaper.cpp" line="98"/>
+        <location filename="../OneClickBingWallpaper.cpp" line="100"/>
         <source>Specific DE</source>
         <translation>按桌面环境设置</translation>
     </message>
     <message>
-        <location filename="../OneClickBingWallpaper.cpp" line="99"/>
+        <location filename="../OneClickBingWallpaper.cpp" line="101"/>
         <source>Language</source>
         <translation>语言</translation>
     </message>
     <message>
-        <location filename="../OneClickBingWallpaper.cpp" line="154"/>
+        <location filename="../OneClickBingWallpaper.cpp" line="157"/>
         <source>Disable</source>
         <translation>关闭自动更新</translation>
     </message>
     <message>
-        <location filename="../OneClickBingWallpaper.cpp" line="154"/>
+        <location filename="../OneClickBingWallpaper.cpp" line="157"/>
         <source>Every 1 minute</source>
         <translation>每分钟</translation>
     </message>
     <message>
-        <location filename="../OneClickBingWallpaper.cpp" line="154"/>
+        <location filename="../OneClickBingWallpaper.cpp" line="157"/>
         <source>Every 5 minute</source>
         <translation>每 5 分钟</translation>
     </message>
     <message>
-        <location filename="../OneClickBingWallpaper.cpp" line="154"/>
+        <location filename="../OneClickBingWallpaper.cpp" line="157"/>
         <source>Every 10 minute</source>
         <translation>每 10 分钟</translation>
     </message>
     <message>
-        <location filename="../OneClickBingWallpaper.cpp" line="155"/>
+        <location filename="../OneClickBingWallpaper.cpp" line="158"/>
         <source>Every 15 minute</source>
         <translation>每 15 分钟</translation>
     </message>
     <message>
-        <location filename="../OneClickBingWallpaper.cpp" line="155"/>
+        <location filename="../OneClickBingWallpaper.cpp" line="158"/>
         <source>Every 30 minute</source>
         <translation>每 30 分钟</translation>
     </message>
     <message>
-        <location filename="../OneClickBingWallpaper.cpp" line="155"/>
+        <location filename="../OneClickBingWallpaper.cpp" line="158"/>
         <source>Every 60 minute</source>
         <translation>每 60 分钟</translation>
     </message>
     <message>
-        <location filename="../OneClickBingWallpaper.cpp" line="161"/>
+        <location filename="../OneClickBingWallpaper.cpp" line="164"/>
         <source>Lastest</source>
         <translation>用当天的图片设置壁纸</translation>
     </message>
     <message>
-        <location filename="../OneClickBingWallpaper.cpp" line="161"/>
+        <location filename="../OneClickBingWallpaper.cpp" line="164"/>
         <source>Random</source>
         <translation>随机选择图片设置壁纸</translation>
     </message>
     <message>
-        <location filename="../OneClickBingWallpaper.cpp" line="195"/>
-        <location filename="../OneClickBingWallpaper.cpp" line="230"/>
+        <location filename="../OneClickBingWallpaper.cpp" line="170"/>
+        <location filename="../OneClickBingWallpaper.cpp" line="176"/>
+        <source>Auto</source>
+        <translation>自动</translation>
+    </message>
+    <message>
+        <location filename="../OneClickBingWallpaper.cpp" line="211"/>
+        <location filename="../OneClickBingWallpaper.cpp" line="248"/>
         <source>Folder Not Found</source>
         <translation>未找到所需的文件夹</translation>
     </message>
     <message>
-        <location filename="../OneClickBingWallpaper.cpp" line="196"/>
-        <location filename="../OneClickBingWallpaper.cpp" line="231"/>
+        <location filename="../OneClickBingWallpaper.cpp" line="212"/>
+        <location filename="../OneClickBingWallpaper.cpp" line="249"/>
         <source>BingWallpaper folder %1 in home is not exist, tool will create it by default</source>
         <translation>程序将默认创建不存在的文件夹 %1</translation>
     </message>
     <message>
-        <location filename="../OneClickBingWallpaper.cpp" line="204"/>
-        <location filename="../OneClickBingWallpaper.cpp" line="248"/>
+        <location filename="../OneClickBingWallpaper.cpp" line="220"/>
+        <location filename="../OneClickBingWallpaper.cpp" line="266"/>
         <source>Attention! Empty Folder</source>
         <translation>注意！空文件夹</translation>
     </message>
     <message>
-        <location filename="../OneClickBingWallpaper.cpp" line="205"/>
+        <location filename="../OneClickBingWallpaper.cpp" line="221"/>
         <source>Empty folder for %1 ,
 if you want to update wallpaper for random, please make sure to put some image file in this folder, or use lastest first</source>
         <translation>%1 文件夹为空。
 如果想随机使用图片更新壁纸，请确认该文件夹中有图片文件，或者先用当天图片更新</translation>
     </message>
     <message>
-        <location filename="../OneClickBingWallpaper.cpp" line="241"/>
+        <location filename="../OneClickBingWallpaper.cpp" line="259"/>
         <source>Python File Not Found</source>
         <translation>未找到 Pyhon 文件</translation>
     </message>
     <message>
-        <location filename="../OneClickBingWallpaper.cpp" line="241"/>
+        <location filename="../OneClickBingWallpaper.cpp" line="259"/>
         <source>Python file not found,please reinstall.</source>
         <translation>未找到 Pyhon 文件，请重装程序。</translation>
     </message>
     <message>
-        <location filename="../OneClickBingWallpaper.cpp" line="249"/>
+        <location filename="../OneClickBingWallpaper.cpp" line="267"/>
         <source>Empty folder for %1.
 If you want to update wallpaper for random, please make sure to put some image file in this folder, or use lastest first.
 Setting Abort, click YES to open folder, NO to ignore.</source>
@@ -123,19 +129,44 @@ Setting Abort, click YES to open folder, NO to ignore.</source>
 本次设置终止，点击 Yes 打开文件夹，点击 No 忽略。</translation>
     </message>
     <message>
-        <location filename="../OneClickBingWallpaper.cpp" line="273"/>
+        <location filename="../OneClickBingWallpaper.cpp" line="291"/>
         <source>Python File Have Modified</source>
         <translation>Python 文件已被修改</translation>
     </message>
     <message>
-        <location filename="../OneClickBingWallpaper.cpp" line="274"/>
+        <location filename="../OneClickBingWallpaper.cpp" line="292"/>
         <source>Python File Have Modified
 Click YES to ignore it</source>
         <translation>Python 文件已被修改
 点击 Yes 跳过检查设置文件</translation>
     </message>
     <message>
-        <location filename="../OneClickBingWallpaper.cpp" line="333"/>
+        <location filename="../OneClickBingWallpaper.cpp" line="329"/>
+        <source>Error</source>
+        <translation>错误</translation>
+    </message>
+    <message>
+        <location filename="../OneClickBingWallpaper.cpp" line="331"/>
+        <source>Something wrong </source>
+        <translation>设置过程出错 </translation>
+    </message>
+    <message>
+        <location filename="../OneClickBingWallpaper.cpp" line="332"/>
+        <source>Please check/adjust your setting or network connection, or just report this issue to maintainer with the following log.</source>
+        <translation>请调整您的设置或者检查网络连接情况，或者用以下日志向维护者报告这个问题。</translation>
+    </message>
+    <message>
+        <location filename="../OneClickBingWallpaper.cpp" line="354"/>
+        <source>Restart required</source>
+        <translation>需要重启程序</translation>
+    </message>
+    <message>
+        <location filename="../OneClickBingWallpaper.cpp" line="355"/>
+        <source>Language update require restart. if update fail, please quit and restart manually</source>
+        <translation>更新语言需要重启程序，如更新失败，请手动退出再重开程序</translation>
+    </message>
+    <message>
+        <location filename="../OneClickBingWallpaper.cpp" line="388"/>
         <source>A tool to set lastest Bingwallpaper in Linux desktop.</source>
         <translation>一个在 Linux 桌面环境上设置最新必应壁纸的工具。</translation>
     </message>
@@ -145,47 +176,47 @@ Click YES to ignore it</source>
     <message>
         <location filename="../OneClickBingWallpaper.h" line="38"/>
         <source>Cinnamon</source>
-        <translation></translation>
+        <translation>Cinnamon</translation>
     </message>
     <message>
         <location filename="../OneClickBingWallpaper.h" line="38"/>
         <source>Deepin</source>
-        <translation></translation>
+        <translation>Deepin</translation>
     </message>
     <message>
         <location filename="../OneClickBingWallpaper.h" line="39"/>
         <source>Gnome</source>
-        <translation></translation>
+        <translation>Gnome</translation>
     </message>
     <message>
         <location filename="../OneClickBingWallpaper.h" line="39"/>
         <source>KDE</source>
-        <translation></translation>
+        <translation>KDE</translation>
     </message>
     <message>
         <location filename="../OneClickBingWallpaper.h" line="39"/>
         <source>Mate</source>
-        <translation></translation>
+        <translation>Mate</translation>
     </message>
     <message>
         <location filename="../OneClickBingWallpaper.h" line="40"/>
         <source>WM</source>
-        <translation></translation>
+        <translation>WM</translation>
     </message>
     <message>
         <location filename="../OneClickBingWallpaper.h" line="40"/>
         <source>Xfce</source>
-        <translation></translation>
+        <translation>Xfce</translation>
     </message>
     <message>
         <location filename="../OneClickBingWallpaper.h" line="44"/>
         <source>简体中文</source>
-        <translation></translation>
+        <translation>简体中文</translation>
     </message>
     <message>
         <location filename="../OneClickBingWallpaper.h" line="44"/>
         <source>English</source>
-        <translation></translation>
+        <translation>English</translation>
     </message>
     <message>
         <location filename="../OneClickBingWallpaperTranslation.h" line="8"/>
@@ -204,31 +235,66 @@ Click YES to ignore it</source>
     </message>
     <message>
         <location filename="../OneClickBingWallpaperTranslation.h" line="11"/>
+        <source>Desktop Environment</source>
+        <translation>桌面环境</translation>
+    </message>
+    <message>
+        <location filename="../OneClickBingWallpaperTranslation.h" line="12"/>
+        <source>Notification</source>
+        <translation>通知</translation>
+    </message>
+    <message>
+        <location filename="../OneClickBingWallpaperTranslation.h" line="13"/>
+        <source>Domain</source>
+        <translation>域名</translation>
+    </message>
+    <message>
+        <location filename="../OneClickBingWallpaperTranslation.h" line="14"/>
         <source>Auto Update</source>
         <translation>自动更新壁纸</translation>
     </message>
     <message>
-        <location filename="../OneClickBingWallpaperTranslation.h" line="12"/>
+        <location filename="../OneClickBingWallpaperTranslation.h" line="15"/>
         <source>Interval</source>
         <translation>更新间隔</translation>
     </message>
     <message>
-        <location filename="../OneClickBingWallpaperTranslation.h" line="13"/>
+        <location filename="../OneClickBingWallpaperTranslation.h" line="16"/>
         <source>Security</source>
         <translation>安全</translation>
     </message>
     <message>
-        <location filename="../OneClickBingWallpaperTranslation.h" line="15"/>
+        <location filename="../OneClickBingWallpaperTranslation.h" line="17"/>
+        <source>Other</source>
+        <translation>其他</translation>
+    </message>
+    <message>
+        <location filename="../OneClickBingWallpaperTranslation.h" line="19"/>
+        <source>Send Notification</source>
+        <translation>发送通知</translation>
+    </message>
+    <message>
+        <location filename="../OneClickBingWallpaperTranslation.h" line="20"/>
+        <source>Subdomain for bing.com</source>
+        <translation>所用的 bing.com 子域名</translation>
+    </message>
+    <message>
+        <location filename="../OneClickBingWallpaperTranslation.h" line="21"/>
         <source>Autoupdate Bingwallpaper when tool startup</source>
         <translation>当程序启动时自动更新壁纸</translation>
     </message>
     <message>
-        <location filename="../OneClickBingWallpaperTranslation.h" line="16"/>
+        <location filename="../OneClickBingWallpaperTranslation.h" line="22"/>
         <source>Check setting Python file&apos;s MD5</source>
         <translation>检查设置壁纸的 Python 文件是否被修改</translation>
     </message>
     <message>
-        <location filename="../OneClickBingWallpaperTranslation.h" line="18"/>
+        <location filename="../OneClickBingWallpaperTranslation.h" line="23"/>
+        <source>Show hint of restart for updating language</source>
+        <translation>显示语言切换时的重启提示</translation>
+    </message>
+    <message>
+        <location filename="../OneClickBingWallpaperTranslation.h" line="25"/>
         <source>Restore Defaults</source>
         <translation>恢复默认设置</translation>
     </message>
